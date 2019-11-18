3f7e72f03eb664bb31513a80ff49d31b6822d052
stephane montoro <stephane.montoro@hotmail.com>
2019-11-18 17:39:30 +0100

    ex is over

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,30 @@
 class GamesController < ApplicationController
   def new
-    @letters = (0...10).map { ('a'..'z').to_a[rand(26)] }.join.upcase
+  @letters = (0...10).map { ('a'..'z').to_a[rand(26)] }.join.upcase
   end
 
   def score
+    @letters = (0...10).map { ('a'..'z').to_a[rand(26)] }.join.upcase
+
     @user_word = params[:user_word]
 
     url = "https://wagon-dictionary.herokuapp.com/#{@user_word}"
     user_serialized = open(url).read
+    test = nil
     @user = JSON.parse(user_serialized)
 
+    if JSON.parse(user_serialized)['found']
+      @user_word.chars.each do |letter|
+        @letters.include?(letter) ? @letters.delete(letter) : test = false
+      end
+      if test == true
+        return @message = 'well done' if test
+      elsif test == false
+        return @message = 'not in the grid'
+      else
+        return @message = 'not an english word'
+      end
+
+    end
   end
 end
