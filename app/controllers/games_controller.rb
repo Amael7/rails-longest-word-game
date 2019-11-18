8f17c7d0f267df416eef28f9d15333c989857187
stephane montoro <stephane.montoro@hotmail.com>
2019-11-18 16:54:03 +0100

    ex is over

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,16 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
+    @letters = (0...10).map { ('a'..'z').to_a[rand(26)] }.join.upcase
   end
 
   def score
+    @user_word = params[:user_word]
+
+    url = "https://wagon-dictionary.herokuapp.com/#{@user_word}"
+    user_serialized = open(url).read
+    @user = JSON.parse(user_serialized)
   end
 end
